73e245cd2d8b472b863b59548ecf390933006fae
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-07-07 18:55:52 -0300

    Migrate test 07 to MXCube 6.15.0

--- SDK/HAL/STM32H743/07-ADC_Test/Core/Src/main.c
+++ SDK/HAL/STM32H743/07-ADC_Test/Core/Src/main.c
@@ -20,7 +20,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "adc.h"
-#include "memorymap.h"
 #include "spi.h"
 #include "tim.h"
 #include "gpio.h"
@@ -307,8 +306,7 @@ void Error_Handler(void)
 		LED_Blink(500);
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
