a543b73b9684167b64c42bbf8cbcaeb412648696
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-07-07 18:54:04 -0300

    Migrate test 04 to MXCube 6.15.0

--- SDK/HAL/STM32H743/04-SD_Test/Core/Src/main.c
+++ SDK/HAL/STM32H743/04-SD_Test/Core/Src/main.c
@@ -19,13 +19,12 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "memorymap.h"
 #include "sdmmc.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "usb_device.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -263,8 +262,7 @@ void Error_Handler(void)
 	}
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
